a1922a6512b8e07c4fe9159c9be75e89bf8ac488
lucalarsen <lucalarsen@hotmail.com>
2019-02-24 13:30:05 +0100

    tilføjet kommentar på abstract klasse

--- BilletLibrary/BilletLibrary/TransportMiddel.cs
+++ BilletLibrary/BilletLibrary/TransportMiddel.cs
@@ -9,11 +9,26 @@ namespace BilletLibrary
     /// </summary>
     public abstract class TransportMiddel
     {
+        /// <summary>
+        /// nummerpladen på et køretøj
+        /// </summary>
         public string Nummerplade { get; set; }
+
+        /// <summary>
+        /// datoen på billet køb
+        /// </summary>
         public DateTime dato { get; set; }
 
+        /// <summary>
+        /// abstract metode til pris på et køretøj
+        /// </summary>
+        /// <returns>retunere pris</returns>
         public abstract decimal Pris();
 
+        /// <summary>
+        /// abstract metode til navn på et køretøj
+        /// </summary>
+        /// <returns></returns>
         public abstract string Køretøj();
     }
 }
