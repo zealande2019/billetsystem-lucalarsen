7b6c4d014acb321651a1e311023afa1a9aee5695
lucalarsen <lucalarsen@hotmail.com>
2019-02-27 10:15:40 +0100

    startet på opgave 9

---
+++ StoreBealtBilletLibrary/StoreBealtBilletLibrary/StoreBealtWeekendRabat.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using BilletLibrary;
+
+namespace StoreBealtBilletLibrary
+{
+    public class StoreBealtWeekendRabat
+    {
+        
+        //public decimal WeekendRabat()
+        //{
+        //    if ()
+        //    {
+
+        //    }
+        //    else
+        //    {
+        //        return BilletLibrary.Bil.Pris();
+        //    }
+
+            
+
+
+        //}
+    }
+}
